34d5ec64cfe7b7a2077a9a4a647dc0a804be74d7
zbralex <5998971@gmail.com>
2017-02-04 15:15:04 +0300

    Не получается менять фильтры, только хром применяется

--- js/form.js
+++ js/form.js
@@ -1,8 +1,8 @@
 'use strict';
- var uploadOverlay = document.querySelector('.upload-overlay');
- uploadOverlay.classList.add('invisible');
- var uploadSelectImage = document.querySelector('#upload-select-image');
- uploadSelectImage.classList.remove('invisible');
+var uploadOverlay = document.querySelector('.upload-overlay');
+uploadOverlay.classList.add('invisible');
+var uploadSelectImage = document.querySelector('#upload-select-image');
+uploadSelectImage.classList.remove('invisible');
 var uploadFileChange = document.querySelector('#upload-file');
 uploadFileChange.addEventListener('click', function () {
   uploadOverlay.classList.remove('invisible');
@@ -11,4 +11,7 @@ var uploadFormCancel = document.querySelector('.upload-form-cancel');
 uploadFormCancel.addEventListener('click', function () {
   uploadOverlay.classList.add('invisible');
 });
-
+var filterImagePreview = document.querySelector('.upload-filter');
+filterImagePreview.addEventListener('click', function () {
+  filterImagePreview.classList.add('filter-chrome');
+});
