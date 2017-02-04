29c0568dc6ec456c6fa3605a245c0f9358aebcc1
zbralex <5998971@gmail.com>
2017-02-04 13:53:50 +0300

    Научился скрывать форму кадрирования

--- js/form.js
+++ js/form.js
@@ -3,7 +3,12 @@
  uploadOverlay.classList.add('invisible');
  var uploadSelectImage = document.querySelector('#upload-select-image');
  uploadSelectImage.classList.remove('invisible');
- var uploadFileChange = document.querySelector('#upload-file');
- uploadFileChange.addEventListener('click', function () {
-   uploadSelectImage.classList.add('invisible');
- });
+var uploadFileChange = document.querySelector('#upload-file');
+uploadFileChange.addEventListener('click', function () {
+  uploadOverlay.classList.remove('invisible');
+});
+var uploadFormCancel = document.querySelector('.upload-form-cancel');
+uploadFormCancel.addEventListener('click', function () {
+  uploadOverlay.classList.add('invisible');
+});
+
