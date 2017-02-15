d6c21673bdabe3059668aa61e78d7c8204a85c2f
zbralex <5998971@gmail.com>
2017-02-15 10:43:55 +0300

    #14 Личный проект: Делегируй! - все работает

--- js/form.js
+++ js/form.js
@@ -8,7 +8,7 @@ var uploadSelectImage = document.querySelector('#upload-select-image');
 uploadSelectImage.classList.remove('invisible');
 var uploadFile = document.querySelector('#upload-file');
 uploadFile.addEventListener('change', function () {
-  uploadOverlay.classList.remove('invisible');
+
 });
 var uploadFormCancel = document.querySelector('.upload-form-cancel');
 uploadFormCancel.addEventListener('click', function () {
@@ -32,55 +32,75 @@ uploadSelectImage.addEventListener('keydown', function (evt) {
 });
 
 // применение фильтров
-  var filterImagePreview = document.querySelector('.filter-image-preview');
-  var uploadFilterForm = document.querySelector('.upload-filter');
-  var prevFilterClass;
-  uploadFilterForm.addEventListener('change', function () {
-    var selectedFilter = uploadFilterForm['upload-filter'].value;
-    var filterCssClass = 'filter-' + selectedFilter;
-    if (prevFilterClass) {
-      filterImagePreview.classList.remove(prevFilterClass);
-    }
-    prevFilterClass = filterCssClass;
-    filterImagePreview.classList.add(filterCssClass);
-  });
+var filterImagePreview = document.querySelector('.filter-image-preview');
+var uploadFilterForm = document.querySelector('.upload-filter');
+var prevFilterClass;
+var prevAreaPressedLabel;
 
+// пресловутый чейндж - выбор нужного фильтра
+uploadFilterForm.addEventListener('change', onFilterChange);
+
+// функция выбора фильтра - код оптимизирован из отдельных переменных в отдельную ф-ю
+function onFilterChange() {
+  var selectedFilter = uploadFilterForm['upload-filter'].value;
+  var filterCssClass = 'filter-' + selectedFilter;
+  if (prevFilterClass) {
+    filterImagePreview.classList.remove(prevFilterClass);
+  }
+  prevFilterClass = filterCssClass;
+  filterImagePreview.classList.add(filterCssClass);
+  toggleAreaPressed(selectedFilter);
+}
+// обновляем атрибуты area-pressed
+function toggleAreaPressed(selectedFilter) {
+  if (prevAreaPressedLabel) {
+    prevAreaPressedLabel.setAttribute('area-pressed', 'false');
+  }
+  var labelElement = document.querySelector('.upload-filter-label-' + selectedFilter);
+  labelElement.setAttribute('area-pressed', 'true');
+  prevAreaPressedLabel = labelElement;
+}
 
 // применение фильтров клавишей enter
-uploadFilterForm.addEventListener('keydown', uploadFilterForm);
-
-
+var uploadFilterControls = document.querySelector('.upload-filter-controls');
+uploadFilterControls.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY_CODE) {
+    var inputElement = event.target.previousElementSibling;
+    uploadFilterForm['upload-filter'].value = inputElement.getAttribute('value');
+    onFilterChange();
+  }
+});
 
 // РЕСАЙЗ
-  var resizeButtonDec = document.querySelector('.upload-resize-controls-button-dec');
-  var resizeButtonInc = document.querySelector('.upload-resize-controls-button-inc');
-  var valueElement = document.querySelector('.upload-resize-controls-value');
+var resizeButtonDec = document.querySelector('.upload-resize-controls-button-dec');
+var resizeButtonInc = document.querySelector('.upload-resize-controls-button-inc');
+var valueElement = document.querySelector('.upload-resize-controls-value');
 
-  resizeButtonDec.addEventListener('click', function () {
-    var newScale = getElementScaleValue() - 25;
-    if (newScale <= 25) {
-      newScale = 25;
-    }
-    setScaleToElement(newScale);
-    updatePreviewScale();
-  });
-  resizeButtonInc.addEventListener('click', function () {
-    var newScale = getElementScaleValue() + 25;
-    if (newScale >= 100) {
-      newScale = 100;
-    }
-    setScaleToElement(newScale);
-    updatePreviewScale();
-  });
-
-  function updatePreviewScale() {
-    filterImagePreview.style.transform = 'scale(' + (getElementScaleValue() / 100) + ')';
+resizeButtonDec.addEventListener('click', function () {
+  var newScale = getElementScaleValue() - 25;
+  if (newScale <= 25) {
+    newScale = 25;
   }
-
-  function getElementScaleValue() {
-    return parseInt(valueElement.value, 10);
+  setScaleToElement(newScale);
+  updatePreviewScale();
+});
+resizeButtonInc.addEventListener('click', function () {
+  var newScale = getElementScaleValue() + 25;
+  if (newScale >= 100) {
+    newScale = 100;
   }
+  setScaleToElement(newScale);
+  updatePreviewScale();
+});
 
-  function setScaleToElement(value) {
-    valueElement.value = value + '%';
-  }
+function updatePreviewScale() {
+  filterImagePreview.style.transform = 'scale(' + (getElementScaleValue() / 100) + ')';
+}
+
+function getElementScaleValue() {
+  return parseInt(valueElement.value, 10);
+}
+
+function setScaleToElement(value) {
+  valueElement.value = value + '%';
+}
