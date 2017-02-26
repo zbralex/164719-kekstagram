a24f84ff7cd850563893b40d5b748bb465c01399
zbralex <5998971@gmail.com>
2017-02-26 23:28:09 +0300

    фиксы

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 'use strict';
 var ESCAPE_KEY_CODE = 27;
 var ENTER_KEY_CODE = 13;
-// resize variables
+
 var filterImagePreview = document.querySelector('.filter-image-preview');
 var uploadResizeControls = document.querySelector('.upload-resize-controls');
 var uploadOverlay = document.querySelector('.upload-overlay');
@@ -15,22 +15,19 @@ uploadFormCancel.addEventListener('click', function () {
   uploadOverlay.classList.add('invisible');
   uploadSelectImage.classList.remove('invisible');
 });
-// переменная, которая ищет по документу класс селектора .upload-filter
+
 var uploadFilterForm = document.querySelector('.upload-filter');
-// переменная, которая явл-ся функцией с объявленными параметрами newFilter, oldFilter
-// - новый примененный фильтр и старый
-// filterImagePreview - это переменная для сужения поиска,
-// не повсему документу, а по конкретному классу .filter-image-preview
+
 var applyFilter = function (newFilter, oldFilter) {
   filterImagePreview.classList.remove('filter-' + oldFilter);
   filterImagePreview.classList.add('filter-' + newFilter);
 };
-// функция показа элемента
+
 function showElementOverlay() {
   uploadSelectImage.classList.add('invisible');
   uploadOverlay.classList.remove('invisible');
 }
-// функция скрытия элемента
+
 function hideElementOverlay() {
   uploadSelectImage.classList.remove('invisible');
   uploadOverlay.classList.add('invisible');
@@ -51,6 +48,6 @@ function onScaleChanged(newScale) {
   filterImagePreview.style.transform = 'scale(' + newScale + ')';
 }
 window.initializeScale(uploadResizeControls, 25, 1, onScaleChanged);
-//
+
 window.initializeFilters(uploadFilterForm, applyFilter);
 
