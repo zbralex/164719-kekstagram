4375024f715fb3d5da36192dd5ea0bcd5572e7b2
zbralex <5998971@gmail.com>
2017-02-26 23:08:06 +0300

    финальный пуш

--- js/load.js
+++ js/load.js
@@ -1,27 +1,16 @@
 'use strict';
 (function () {
-  window.load = function () {
-    var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
-    var errorHandler = function (err) {
-    };
-    return function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      if (typeof onError === 'function') {
-        errorHandler = onError;
-      }
+  var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
 
-      xhr.addEventListener('load', function (evt) {
-        if (evt.target.status >= 400) {
-          errorHandler('Failed to load data. Server returned status: ' + evt.target.status);
-        } else if (evt.target.status >= 200) {
-          onLoad(evt.target.response);
-        }
-      });
-      xhr.addEventListener('error', errorHandler);
-      xhr.addEventListener('timeout', errorHandler);
-      xhr.responseType = 'json';
-      xhr.open('GET', DATA_URL);
-      xhr.send();
-    };
+  window.load = function (onLoad) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function (evt) {
+      if (evt.target.status >= 200) {
+        onLoad(evt.target.response);
+      }
+    });
+    xhr.responseType = 'json';
+    xhr.open('GET', DATA_URL);
+    xhr.send();
   };
 })();
--- js/pictures.js
+++ js/pictures.js
@@ -1,24 +1,28 @@
 'use strict';
 
 (function () {
-  var pictures = [];
-  var comments = [];
+  var originalData = [];
+
+  var templateElement = document.querySelector('#picture-template');
+  var elementToClone = templateElement.content.querySelector('.picture');
+  var filterElement = document.querySelector('.filters');
+  var picturesContainerElement = document.querySelector('.pictures.container');
 
   function onLoad(data) {
-    pictures = data;
-    var templateElement = document.querySelector('#picture-template');
-    var picturesContainerElement = document.querySelector('.pictures.container');
-    comments.forEach(function () {
-      var elementToClone = templateElement.content.querySelector('.gallery-overlay-controls-comments');
-      var commentElement = elementToClone.cloneNode(true);
-      commentElement.setAttribute('.comments-count');
-    });
+    originalData = data;
+    filterElement.classList.remove('hidden');
+    renderPictures(originalData);
+  }
 
-    pictures.forEach(function (pictureData) {
-      var elementToClone = templateElement.content.querySelector('.picture');
+  function renderPictures(picturesData) {
+    picturesContainerElement.innerHTML = '';
+
+    picturesData.forEach(function (pictureData) {
       var pictureElement = elementToClone.cloneNode(true);
       pictureElement.setAttribute('href', pictureData.url);
       pictureElement.querySelector('img').setAttribute('src', pictureData.url);
+      pictureElement.querySelector('.picture-comments').innerText = pictureData.comments.length;
+      pictureElement.querySelector('.picture-likes').innerText = pictureData.likes;
 
       pictureElement.addEventListener('click', function (evt) {
         evt.preventDefault();
@@ -28,5 +32,63 @@
       picturesContainerElement.appendChild(pictureElement);
     });
   }
-  window.load()(onLoad);
+
+
+  filterElement.addEventListener('change', function () {
+    switch (getFilterValue()) {
+      case 'new':
+        renderPictures(getRandomNewPictures());
+        break;
+      case 'discussed':
+        renderPictures(getSortedByCommentsPictures());
+        break;
+      case 'popular':
+        renderPictures(originalData);
+        break;
+    }
+  });
+
+  function getFilterValue() {
+    for (var i = 0; i < filterElement['filter'].length; i++) {
+      var inputElement = filterElement['filter'][i];
+      if (inputElement.checked) {
+        return inputElement.value;
+      }
+    }
+    return null;
+  }
+
+  function getRandomNewPictures() {
+    return shuffle(originalData).slice(0, 10);
+  }
+
+  function shuffle(a) {
+    var shuffled = a.slice();
+    var j;
+    var x;
+    var i;
+    for (i = shuffled.length; i; i--) {
+      j = Math.floor(Math.random() * i);
+      x = shuffled[i - 1];
+      shuffled[i - 1] = shuffled[j];
+      shuffled[j] = x;
+    }
+    return shuffled;
+  }
+
+  function getSortedByCommentsPictures() {
+    function compare(a, b) {
+      if (a.comments.length < b.comments.length) {
+        return 1;
+      }
+      if (a.comments.length > b.comments.length) {
+        return -1;
+      }
+      return 0;
+    }
+
+    return originalData.slice().sort(compare);
+  }
+
+  window.load(onLoad);
 })();
