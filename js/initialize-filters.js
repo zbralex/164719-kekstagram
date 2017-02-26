01d43a5be6cf696da9ef96d79067d38b70f7f198
Alexander Zubritsky <5998971@gmail.com>
2017-02-26 14:50:27 +0300

    комментарии в коде

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,22 +1,28 @@
 'use strict';
 (function () {
-  window.initializeFilters = function () {
-    var filterImagePreview = document.querySelector('.filter-image-preview');
-    var uploadFilterForm = document.querySelector('.upload-filter');
-    var onFilterChange;
+  // функция с глобальной областью видимости и переданнами и двумя переданными аргументами uploadFilterForm, applyFilter
+  window.initializeFilters = function (uploadFilterForm, applyFilter) {
+    var prevFilterClass;
     var prevAreaPressedLabel;
     var ENTER_KEY_CODE = 13;
-    var selectedFilter = uploadFilterForm['upload-filter'].value;
-
-
-    var applyFilter = function (newFilter, oldFilter) {
-      filterImagePreview.classList.remove('filter-' + oldFilter);
-      filterImagePreview.classList.add('filter-' + newFilter);
-      toggleAreaPressed(selectedFilter);
-      initializeFilters(onFilterChange, applyFilter);
-    };
+    // пресловутый чейндж - выбор нужного фильтра
     uploadFilterForm.addEventListener('change', onFilterChange);
-    function toggleAreaPressed() {
+
+    // функция выбора фильтра - код оптимизирован из отдельных переменных в отдельную ф-ю
+    function onFilterChange() {
+      // ниже переменная для поиска по инпутам, в них добавляются значения value (или имена фильтров)
+      var selectedFilter = uploadFilterForm['upload-filter'].value;
+      // ниже присваиваем значению новой переменной filterCssClass значение переменной selectedFilter
+      var filterCssClass =  selectedFilter;
+      // ниже идет переменная, объявленная в файле form.js, которая является функцией с переданными агрументами
+      // filterCssClass, prevFilterClass || newFilter, oldFilter
+      applyFilter(filterCssClass, prevFilterClass);
+      // ниже присваваем значению переменной prevFilterClass зачение переменной filterCssClass
+      prevFilterClass = filterCssClass;
+      toggleAreaPressed(selectedFilter);
+    }
+    // обновляем атрибуты area-pressed
+    function toggleAreaPressed(selectedFilter) {
       if (prevAreaPressedLabel) {
         prevAreaPressedLabel.setAttribute('area-pressed', 'false');
       }
@@ -24,7 +30,8 @@
       labelElement.setAttribute('area-pressed', 'true');
       prevAreaPressedLabel = labelElement;
     }
-// применение фильтров клавишей enter
+
+    // применение фильтров клавишей enter
     var uploadFilterControls = document.querySelector('.upload-filter-controls');
     uploadFilterControls.addEventListener('keydown', function (evt) {
       if (evt.keyCode === ENTER_KEY_CODE) {
@@ -33,5 +40,5 @@
         onFilterChange();
       }
     });
-  }
+  };
 })();
