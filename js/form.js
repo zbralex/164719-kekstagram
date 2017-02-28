ea37724fe478b7178e9f6eb117998930fca27a53
zbralex <5998971@gmail.com>
2017-02-28 08:45:57 +0300

    IIFE и переименованная ф-ция

--- js/form.js
+++ js/form.js
@@ -1,53 +1,59 @@
 'use strict';
-var ESCAPE_KEY_CODE = 27;
-var ENTER_KEY_CODE = 13;
 
-var filterImagePreview = document.querySelector('.filter-image-preview');
-var uploadResizeControls = document.querySelector('.upload-resize-controls');
-var uploadOverlay = document.querySelector('.upload-overlay');
-uploadOverlay.classList.add('invisible');
-var uploadSelectImage = document.querySelector('#upload-select-image');
-uploadSelectImage.classList.remove('invisible');
-var uploadFile = document.querySelector('#upload-file');
-uploadFile.addEventListener('change', showElementOverlay);
-var uploadFormCancel = document.querySelector('.upload-form-cancel');
-uploadFormCancel.addEventListener('click', function () {
+(function () {
+  var ESCAPE_KEY_CODE = 27;
+  var ENTER_KEY_CODE = 13;
+
+  var filterImagePreview = document.querySelector('.filter-image-preview');
+  var uploadResizeControls = document.querySelector('.upload-resize-controls');
+  var uploadOverlay = document.querySelector('.upload-overlay');
   uploadOverlay.classList.add('invisible');
+  var uploadSelectImage = document.querySelector('#upload-select-image');
   uploadSelectImage.classList.remove('invisible');
-});
+  var uploadFile = document.querySelector('#upload-file');
+  uploadFile.addEventListener('change', showElementOverlay);
+  var uploadFormCancel = document.querySelector('.upload-form-cancel');
+  uploadFormCancel.addEventListener('click', function () {
+    uploadOverlay.classList.add('invisible');
+    uploadSelectImage.classList.remove('invisible');
+  });
 
-var uploadFilterForm = document.querySelector('.upload-filter');
+  var uploadFilterForm = document.querySelector('.upload-filter');
 
-var applyFilter = function (newFilter, oldFilter) {
-  filterImagePreview.classList.remove('filter-' + oldFilter);
-  filterImagePreview.classList.add('filter-' + newFilter);
-};
+  var applyFilter = function (newFilter, oldFilter) {
+    filterImagePreview.classList.remove('filter-' + oldFilter);
+    filterImagePreview.classList.add('filter-' + newFilter);
+  };
 
-function showElementOverlay() {
-  uploadSelectImage.classList.add('invisible');
-  uploadOverlay.classList.remove('invisible');
-}
-
-function hideElementOverlay() {
-  uploadSelectImage.classList.remove('invisible');
-  uploadOverlay.classList.add('invisible');
-  document.removeEventListener('keydown', tryHideElementOverlay);
-}
-function tryHideElementOverlay(evt) {
-  if (evt.keyCode === ESCAPE_KEY_CODE) {
-    hideElementOverlay();
+  function showElementOverlay() {
+    uploadSelectImage.classList.add('invisible');
+    uploadOverlay.classList.remove('invisible');
   }
-}
-uploadSelectImage.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEY_CODE) {
-    showElementOverlay();
-    document.addEventListener('keydown', tryHideElementOverlay);
+
+  function hideElementOverlay() {
+    uploadSelectImage.classList.remove('invisible');
+    uploadOverlay.classList.add('invisible');
+    document.removeEventListener('keydown', onDocumentKeydown);
   }
-});
-function onScaleChanged(newScale) {
-  filterImagePreview.style.transform = 'scale(' + newScale + ')';
-}
-window.initializeScale(uploadResizeControls, 25, 1, onScaleChanged);
 
-window.initializeFilters(uploadFilterForm, applyFilter);
+  function onDocumentKeydown(evt) {
+    if (evt.keyCode === ESCAPE_KEY_CODE) {
+      hideElementOverlay();
+    }
+  }
 
+  uploadSelectImage.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEY_CODE) {
+      showElementOverlay();
+      document.addEventListener('keydown', onDocumentKeydown);
+    }
+  });
+  function onScaleChanged(newScale) {
+    filterImagePreview.style.transform = 'scale(' + newScale + ')';
+  }
+
+  window.initializeScale(uploadResizeControls, 25, 1, onScaleChanged);
+
+  window.initializeFilters(uploadFilterForm, applyFilter);
+
+})();
